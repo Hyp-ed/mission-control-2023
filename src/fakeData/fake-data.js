a1cc15854903f680ffdb0e9ea4c50da95019963e
Arjun Naha <arjunnaha@users.noreply.github.com>
2022-11-10 18:24:58 +0000

    feat(fake): Add config and interval based publish

--- src/fakeData/fake-data.js
+++ src/fakeData/fake-data.js
@@ -1,28 +1,27 @@
-const random  = require('random')
-const mqtt = require('mqtt')
-const client  = mqtt.connect('ws://broker.emqx.io:8083/mqtt')
+const random = require("random");
+const mqtt = require("mqtt");
+const SENSORS = require('./fake-sensors.json');
+const client = mqtt.connect("ws://broker.emqx.io:8083/mqtt");
 
+client.on("connect", function () {
+  console.log("CLIENT CONNECTED");
 
+  SENSORS.forEach(sensor => {
+    setInterval(() => {
+      const temperature = generateRandomTemperature();
+      client.publish(sensor.topic, temperature);
+      console.log(`Published ${temperature} to ${sensor.topic}`);
+    }, 1000);
+  });
+  client.publish("hyped.temperature", generateRandomTemperature());
+});
 
-client.on('connect', function () {
-    console.log("connectged")
-  client.subscribe('hyped.temperature', function (err) {
-    console.error(err)
-    if (!err) {
-      client.publish('hyped.temperature', generateRandomTemperature())
-    }
-  })
-})
+// client.on("message", function (topic, message) {
+//   console.log(message.toString());
+//   client.end();
+// });
 
-client.on('message', function (topic, message) {
-  // message is Buffer
-
-  console.log(message.toString())
-
-  client.end()
-})
-
-let generateRandomTemperature =  function(){
-    let randomTemperature = Math.round(random.float((min = 0),(max = 100)))
-    return String(randomTemperature)
-}
+let generateRandomTemperature = function () {
+  let randomTemperature = Math.round(random.float((min = 0), (max = 100)));
+  return String(randomTemperature);
+};
